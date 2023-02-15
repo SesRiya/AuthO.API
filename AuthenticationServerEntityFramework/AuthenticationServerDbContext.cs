22719ffa9a2b93034c8725ea786432c3e95f705e
SesRiya <crdgorospe@gmail.com>
2023-02-16 08:48:04 +1300

    updated db

--- AuthenticationServerEntityFramework/AuthenticationServerDbContext.cs
+++ AuthenticationServerEntityFramework/AuthenticationServerDbContext.cs
@@ -8,11 +8,12 @@ namespace AuthenticationServerEntityFramework
         public DbSet<User>? Users { get; set; }
         public DbSet<Role>? Roles { get; set; }
         public DbSet<UserRole>? UserRoles { get; set; }
+        public DbSet<RefreshToken>? RefreshTokens { get; set; }
 
  
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=AuthenticationServerDb;Trusted_Connection=True");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=AuthServerDb;Trusted_Connection=True");
             base.OnConfiguring(optionsBuilder);
         }
 
